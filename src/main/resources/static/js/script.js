90106eb7a5424ff074f9169027fb62df408043ae
LyndseyR <lyndseyroka@Lyndseys-MacBook-Pro.local>
2017-04-09 12:59:21 -0400

    updated JS and links on all pages mobile and header navigation

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,24 +1,8 @@
+// Toggle Menu for Mobile 
+
 $(document).ready(function(){
 
   $('.toggle').click(function(){
     $('.show-toggle').slideToggle();
   });
 });
-<<<<<<< HEAD
-
-
-
-// $(document).ready(function(){
-//   var timer = 5;
-//   setInterval(function () {
-//     timer--;
-//     if(timer > 0) {
-//       $('.timer').html(timer);
-//     }
-//     else {
-//       window.location = "end.html";
-//     }
-//   }, 1000);
-// });
-=======
->>>>>>> origin/cmv1.0
